9e75f99e8be8bb8256649b47ea39b34a23b873d7
aayushkarki682 <ayush.karki12345@gmail.com>
2021-03-11 03:34:55 -0600

    adding controller and returning flux in browser

---
+++ src/main/java/com/reactiveprogramming/controllers/FluxAndMonoController.java
@@ -0,0 +1,31 @@
+package com.reactiveprogramming.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> returnFlux(){
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                //When we do this it takes 4 seconds to finally show it in the browser.
+                //it happens because browser is not a non-blocking pane
+                //it only cares about what format of data is going to be shown
+                //so it waits until it gets all the data from the server and then only prints it in the browser.
+                .log();
+    }
+
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    //Providing the return value of stream of JSON value, the browser renders the value one by one
+    public Flux<Integer> returnFluxStream(){
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+}
